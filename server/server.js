9d35f17f59a42649d32de372f5acb70fea19c5b6
Elizabeth Akerman <ekakerman@gmail.com>
2017-01-23 15:35:02 -0800

    updated server functionality

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
-var app = require('./server-config.js');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+//var app = require('./server-config.js');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve('../public/index.html'));
+});
 
 app.listen(3000, function() {
   console.log('Server now listening on port 3000');
 });
+
