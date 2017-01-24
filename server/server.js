a1bd816f621ab18bde7fdea96a8b20405bbb8da1
Elizabeth Akerman <ekakerman@gmail.com>
2017-01-24 10:41:40 -0800

    promisified monogoose request

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var path = require('path');
+var db = require('./db/db.js')
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-//app.use(express.static(path.join(__dirname, 'client')));
 app.use('', express.static('client'));
 
 app.get('/', function (req, res) {
+  //console.log(db.db);
   res.sendFile(path.resolve('./client/index.html'));
+});
+
+app.get('/database', function(req, res) {
+  db.getData(req, res);
 });
 
 app.listen(port, function() {
   console.log('Server now listening on port 3000');
 });
-
