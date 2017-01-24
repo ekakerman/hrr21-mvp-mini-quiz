ae0a260d75df08669cd5e05f1eed8f6b6c4cce86
Elizabeth Akerman <ekakerman@gmail.com>
2017-01-24 10:42:26 -0800

    added partial views for answers

--- client/app.js
+++ client/app.js
@@ -1,1 +1,23 @@
-console.log('hello!');
+angular.module('app', ['ngRoute'])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/answerA', {
+        templateUrl: '/partials/answerA.html',
+        controller: 'answerACtrl'
+      })
+      .when('/answerB', {
+        templateUrl: '/partials/answerB.html',
+        controller: 'answerBCtrl'
+      })
+      .when('/answerC', {
+        templateUrl: '/partials/answerC.html',
+        controller: 'answerCCtrl'
+      })
+      .when('/answerD', {
+        templateUrl: '/partials/answerD.html',
+        controller: 'answerDCtrl'
+      })
+      .otherwise({
+        redirectTo: '/invalid.html'
+      });
+  })
