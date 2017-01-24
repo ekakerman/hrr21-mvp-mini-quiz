01c8ef0798d88df44328c7c89a5dbaed6b79290a
Elizabeth Akerman <ekakerman@gmail.com>
2017-01-24 11:05:12 -0800

    added controllers for partial views

---
+++ client/appControllers.js
@@ -0,0 +1,17 @@
+angular.module('app.answerA', [])
+
+.controller('answerACtrl', function ($scope, $location) {
+  $scope.results = 'ANSWER A RESULTS!';
+})
+
+.controller('answerBCtrl', function ($scope, $location) {
+  $scope.results = 'ANSWER B RESULTS!';
+})
+
+.controller('answerCCtrl', function ($scope, $location) {
+  $scope.results = 'ANSWER C RESULTS!';
+})
+
+.controller('answerDCtrl', function ($scope, $location) {
+  $scope.results = 'ANSWER D RESULTS!';
+})
