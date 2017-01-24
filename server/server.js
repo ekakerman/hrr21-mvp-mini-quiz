bf958b063f5004841d9a0d04548fff2caa06506d
Elizabeth Akerman <ekakerman@gmail.com>
2017-01-23 16:18:46 -0800

    updated port for Heroku

--- server/server.js
+++ server/server.js
@@ -3,13 +3,15 @@ var path = require('path');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve('../client/index.html'));
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log('Server now listening on port 3000');
 });
 
